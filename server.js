db26cbca0fa7a3180c28b2f9bbc362cd34e9998c
AndyChen <shinningstar1001@gmail.com>
2016-01-07 03:20:48 +0800

    test

--- server.js
+++ server.js
@@ -1,36 +1,6 @@
-var express = require("express");
-var multer = require('multer');
-var path = require('path');
-var app = express();
-var storage = multer.diskStorage({
-    destination: function(req, file, callback) {
-        callback(null, './uploads');
-    },
-    filename: function(req, file, callback) {
-        console.log('receive file :' + JSON.stringify(file));
-        callback(null, file.originalname);
-    }
-});
-var upload = multer({
-    storage: storage
-}).single('uploadFile');
-
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + "/index.html");
-});
-
-app.post('/api/upload', function(req, res) {
-    upload(req, res, function(err) {
-        if (err) {
-            return res.end("Error uploading file. Error: " + err);
-        }
-        res.end("File is uploaded");
-    });
-});
-
-app.use(express.static(path.join(__dirname, 'uploads')));
-
-var port = process.env.PORT || 3000;
-app.listen(port, function() {
-    console.log("Working on port :" + port);
-});
+var http = require('http')
+var port = process.env.PORT || 1337;
+http.createServer(function(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World\n');
+}).listen(port);
