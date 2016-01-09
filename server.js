f4b0357a9d62a2ed58f0f2e2c13d115365df8ea3
AndyChen <shinningstar1001@gmail.com>
2016-01-10 01:05:37 +0800

    change upload text

--- server.js
+++ server.js
@@ -1,44 +1,46 @@
-var express = require("express");
+var express = require('express');
 var multer = require('multer');
 var path = require('path');
 var fs = require('fs-extra');
 var app = express();
 var uploadFolderPath = './uploads';
-var storage = multer.diskStorage({    
-    destination: function(req, file, callback) {
-        callback(null, uploadFolderPath);
-    },
-    filename: function(req, file, callback) {
-        console.log('receive file :' + JSON.stringify(file));
-        callback(null, file.originalname);
-    }
+var storage = multer.diskStorage({
+  destination: function(req, file, callback) {
+    callback(null, uploadFolderPath);
+  },
+
+  filename: function(req, file, callback) {
+    console.log('receive file :' + JSON.stringify(file));
+    callback(null, file.originalname);
+  },
 });
 
-fs.ensureDir(uploadFolderPath, function (err) {
-  console.log(err) // => null
+fs.ensureDir(uploadFolderPath, function(err) {
+  console.log(err); // => null
   // dir has now been created, including the directory it is to be placed in
-})
+});
 
 var upload = multer({
-    storage: storage
+  storage: storage,
 }).single('uploadFile');
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.post('/api/upload', function(req, res) {
-    upload(req, res, function(err) {
-        if (err) {
-            return res.end("Error uploading file. Error: " + err);
-        }
-        res.end("File is uploaded");
-    });
+  upload(req, res, function(err) {
+    if (err) {
+      return res.end('Error uploading file. Error: ' + err);
+    }
+
+    res.end('File is uploaded');
+  });
 });
 
 app.use(express.static(path.join(__dirname, 'uploads')));
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
-    console.log("Working on port :" + port);
+  console.log('Working on port :' + port);
 });
