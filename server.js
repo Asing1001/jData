0135e67a184e49b00230d8d4f6fd723c17bc42fe
AndyChen <shinningstar1001@gmail.com>
2016-01-10 14:15:37 +0800

    加上提示訊息

--- server.js
+++ server.js
@@ -5,60 +5,60 @@ var fs = require('fs-extra');
 var app = express();
 var uploadFolderPath = './uploads';
 var storage = multer.diskStorage({
-  destination: function(req, file, callback) {
-    callback(null, uploadFolderPath);
-  },
+    destination: function (req, file, callback) {
+        callback(null, uploadFolderPath);
+    },
 
-  filename: function(req, file, callback) {
-    console.log('receive file :' + JSON.stringify(file));
-    callback(null, file.originalname);
-  },
+    filename: function (req, file, callback) {
+        console.log('receive file :' + JSON.stringify(file));
+        callback(null, file.originalname);
+    },
 });
 
-fs.ensureDir(uploadFolderPath, function(err) {
-  console.log(err); // => null
-  // dir has now been created, including the directory it is to be placed in
+fs.ensureDir(uploadFolderPath, function (err) {
+    console.log(err); // => null
+    // dir has now been created, including the directory it is to be placed in
 });
 
-var maxSize = 5* 1024 * 1024;
+var maxSize = 5 * 1024 * 1024;
 var upload = multer({
-  storage: storage,
-  limits : { fileSize: maxSize},
+    storage: storage,
+    limits: {fileSize: maxSize},
 }).single('uploadFile');
 
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
-app.post('/api/upload', function(req, res) {
-  upload(req, res, function(err) {
-    if (err) {
-      return res.end('Error uploading file. ' + err);
-    }
+app.post('/api/upload', function (req, res) {
+    upload(req, res, function (err) {
+        if (err) {
+            return res.end('Error uploading file. ' + err);
+        }
 
-    res.end('File is uploaded');
-  });
+        res.end('File is uploaded');
+    });
 });
 
-app.get('/list', function(req,res){
-  var items = [];
-  var reg = new RegExp(/uploads$/g);
-  fs.walk(uploadFolderPath)
-      .on('data', function (item) {
-        if(!reg.test(item.path))
-        items.push(item.path)
-      })
-      .on('end', function () {
-        res.json(items) // => [ ... array of files]
-      })
+app.get('/list', function (req, res) {
+    var items = [];
+    var reg = new RegExp(/uploads$/g);
+    fs.walk(uploadFolderPath)
+        .on('data', function (item) {
+            if (!reg.test(item.path))
+                items.push(item.path)
+        })
+        .on('end', function () {
+            res.json(items) // => [ ... array of files]
+        })
 });
 
 app.use(express.static(path.join(__dirname, 'uploads')));
-app.use('/scripts',express.static(path.join(__dirname, '/scripts')));
-app.use('/bower_components',express.static(path.join(__dirname, '/bower_components')));
+app.use('/scripts', express.static(path.join(__dirname, '/scripts')));
+app.use('/bower_components', express.static(path.join(__dirname, '/bower_components')));
 
 
 var port = process.env.PORT || 3000;
-app.listen(port, function() {
-  console.log('Working on port :' + port);
+app.listen(port, function () {
+    console.log('Working on port :' + port);
 });
