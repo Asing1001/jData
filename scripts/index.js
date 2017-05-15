830d5e7abee6f4be86423a0f96310972adbd8f69
Andy Chen <shinningstar1001@gmail.com>
2017-05-15 18:46:48 +0800

    remove format, improve ui

--- scripts/index.js
+++ scripts/index.js
@@ -4,22 +4,24 @@
 fetch('/api/files')
     .then(function(response) {
         return response.json();
-    }).then(function (fileArray) {
-    fileArray.forEach(function (filePath) {
-        var fileName = filePath.substr(filePath.lastIndexOf('\\')+1);
-        var a = $('<a href="/edit/{0}">{0}</a><br/>'.format(fileName));
-        $('#listContainer').append(a);
+    }).then(function(fileArray) {
+        fileArray.forEach(function(filePath) {
+            var fileName = filePath.substr(filePath.lastIndexOf('\\') + 1);
+            var a = $(`<a href="/${fileName}">${fileName}</a>`);
+            var editLink = $(`<a style="margin-left:10px" href="/edit/${fileName}">Edit</a><br/>`);            
+            $('#listContainer').append(a);
+            $('#listContainer').append(editLink);
+        })
     })
-})
 
-var maxSize = 5*1024*1024;
+var maxSize = 5 * 1024 * 1024;
 $('input[name="uploadFile"]').bind('change', function() {
 
     //this.files[0].size gets the size of your file.
-    if(this.files[0].size>maxSize){
+    if (this.files[0].size > maxSize) {
         alert("File should not exceed 5 MB!!");
-        $('#uploadBtn').attr('disabled',true);
-    }else {
+        $('#uploadBtn').attr('disabled', true);
+    } else {
         $('#uploadBtn').removeAttr('disabled');
     }
 });
