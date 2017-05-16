cbb237c842be21387c16f1fed10ca86f73db0b5b
Andy Chen <shinningstar1001@gmail.com>
2017-05-16 15:06:30 +0800

    add delete function

--- routes/api/files.js
+++ routes/api/files.js
@@ -9,96 +9,96 @@ var path = require('path');
 var uploadFolderPath = global.appRoot + '\\uploads\\';
 var multer = require('multer');
 var storage = multer.diskStorage({
-  destination: function(req, file, callback) {
-    callback(null, uploadFolderPath);
-  },
+    destination: function(req, file, callback) {
+        callback(null, uploadFolderPath);
+    },
 
-  filename: function(req, file, callback) {
-    console.log('receive file :' + JSON.stringify(file));
-    callback(null, file.originalname);
-  },
+    filename: function(req, file, callback) {
+        console.log('receive file :' + JSON.stringify(file));
+        callback(null, file.originalname);
+    },
 });
 
 fsExtra.ensureDir(uploadFolderPath, function(err) {
-    if(err){
+    if (err) {
         console.log(err);
     }
 });
 
 var maxSize = 5 * 1024 * 1024;
 var upload = multer({
-  storage: storage,
-  limits: {
-    fileSize: maxSize,
-  },
+    storage: storage,
+    limits: {
+        fileSize: maxSize,
+    },
 }).single('uploadFile');
 
 router.route('/files/:fileName')
     .get(function(req, res) {
-      var filePath = uploadFolderPath + req.params.fileName;
-      var options = {
-        headers: {
-          'Content-Type': 'application/json; charset=utf-8',
-        },
-      };
-      fs.access(filePath, fs.F_OK, function(err) {
-        if (!err) {
-          res.sendFile(filePath,options);
-        } else {
-          res.end('there is no such file!');
-        }
-      });
+        var filePath = uploadFolderPath + req.params.fileName;
+        var options = {
+            headers: {
+                'Content-Type': 'application/json; charset=utf-8',
+            },
+        };
+        fs.access(filePath, fs.F_OK, function(err) {
+            if (!err) {
+                res.sendFile(filePath, options);
+            } else {
+                res.end('there is no such file!');
+            }
+        });
     })
     .put(function(req, res) {
-      var filePath = uploadFolderPath + req.params.fileName;
-      var content = req.body;
+        var filePath = uploadFolderPath + req.params.fileName;
+        var content = req.body;
         fs.writeFile(filePath, content, 'utf8', function(err) {
-        if (!err) {
-          res.end('success edit!');
-        } else {
-          res.end('Error:' + err);
-        }
-      });
+            if (!err) {
+                res.end('success edit!');
+            } else {
+                res.end('Error:' + err);
+            }
+        });
     })
     .delete(function(req, res) {
-      var filePath = uploadFolderPath + req.params.fileName;
-      var pwd = req.body.pwd;
-      var canDelete = pwd === 'pass.123';
-      if (!canDelete) {
-        res.end('you cant use delete function!');
-      }
-
-      fsExtra.remove(filePath, function(err) {
-        if (!err) {
-          res.end('success delete file !');
+        var filePath = uploadFolderPath + req.params.fileName;
+        var pwd = req.body.pwd;
+        var canDelete = pwd === 'pass.123';
+        if (canDelete) {
+            fsExtra.remove(filePath, function(err) {
+                if (!err) {
+                    res.end('success delete file !');
+                } else {
+                    res.end('Error:' + err);
+                }
+            });
         } else {
-          res.end('Error:' + err);
+            res.end('you cant use delete function!');
         }
-      });
     });
 
 router.route('/files')
     .get(function(req, res) {
-      var items = [];
-      var reg = new RegExp(/uploads$/g);
-      fsExtra.walk(uploadFolderPath)
+        var items = [];
+        var reg = new RegExp(/uploads$/g);
+        fsExtra.walk(uploadFolderPath)
             .on('data', function(item) {
-              if (!reg.test(item.path))
-                  items.push(item.path);
+                if (!reg.test(item.path))
+                    items.push(item.path);
             })
             .on('end', function() {
-              res.json(items); // => [ ... array of files]
+                res.json(items); // => [ ... array of files]
             });
     })
     .post(function(req, res) {
 
-      upload(req, res, function(err) {
-        if (err) {
-          return res.end('Error uploading file. ' + err);
-        }
+        upload(req, res, function(err) {
+            if (err) {
+                return res.end('Error uploading file. ' + err);
+            }
 
-        res.end('File is uploaded');
-      });
+            res.end('File is uploaded');
+        });
     });
 
 module.exports = router;
--- scripts/index.js
+++ scripts/index.js
@@ -2,20 +2,22 @@
  * Created by Andy on 2016/1/14.
  */
 fetch('/api/files')
-    .then(function (response) {
+    .then(function(response) {
         return response.json();
-    }).then(function (fileArray) {
-        fileArray.forEach(function (filePath) {
+    }).then(function(fileArray) {
+        fileArray.forEach(function(filePath) {
             var fileName = filePath.substr(filePath.lastIndexOf('\\') + 1);
             var a = $(`<a href="/${fileName}">${fileName}</a>`);
-            var editLink = $(`<a style="margin-left:10px" href="/edit/${fileName}">Edit</a><br/>`);
+            var editLink = $(`<a style="margin-left:10px" href="/edit/${fileName}">Edit</a>`);
+            var deleteBtn = $(`<button class="deleteBtn" type="button" data-file-name="${fileName}">Delete</button><br/>`);
             $('#listContainer').append(a);
             $('#listContainer').append(editLink);
+            $('#listContainer').append(deleteBtn);
         })
     })
 
 var maxSize = 5 * 1024 * 1024;
-$('input[name="uploadFile"]').bind('change', function () {
+$('input[name="uploadFile"]').bind('change', function() {
 
     //this.files[0].size gets the size of your file.
     if (this.files[0].size > maxSize) {
@@ -26,9 +28,25 @@ $('input[name="uploadFile"]').bind('change', function () {
     }
 });
 
+$(document).on('click', '.deleteBtn', (event) => {
+    var pwd = prompt("Please input password to delete : ");
+    if (pwd) {
+        fetch(`/api/files/${event.target.dataset.fileName}`, {
+            method: "delete",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ pwd })
+        }).then(res => res.text()).then(text => {
+            alert(text);
+            location.reload();
+        })
+    }
+})
+
 document.getElementById("createFileBtn").onclick = () => {
     var filename = prompt("Please enter the file name :");
     if (filename) {
-        location.href = `${location.origin}/edit/${filename}`;
+        location.href = `/edit/${filename}`;
     }
 }
